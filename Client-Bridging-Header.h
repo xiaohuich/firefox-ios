0897874b3c9c6ea6f8f8a7bf0dc2807302531d2e
Stephan Leroux <sleroux@mozilla.com>
2017-04-28 15:42:05 -0400

    Removed BB from briding header/cleaned up xcconfig files

--- Client-Bridging-Header.h
+++ Client-Bridging-Header.h
@@ -11,7 +11,6 @@
 #import "ThirdParty/Apple/UIImage+ImageEffects.h"
 
 #import <CrashReporter/CrashReporter.h>
-#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 #import "Shared-Bridging-Header.h"
 #import "Storage-Bridging-Header.h"
